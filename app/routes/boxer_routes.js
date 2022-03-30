c5781da042c591975f293c05159fea6ce5be9d4b
yazeedhani <haniyazeed9@gmail.com>
2022-03-29 19:23:58 -0600

    The app uses RESTful routing, all seven RESTful routes.

--- app/routes/boxer_routes.js
+++ app/routes/boxer_routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 // const passport = require('passport')
 
-// pull in Mongoose model for examples
+// pull in Mongoose model for boxers
 const Boxer = require('../models/boxer')
 
 // this is a collection of methods that help us detect situations when we need
